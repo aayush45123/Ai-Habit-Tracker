8a3e9590ce707e87d09ffbcf5b8f74b095f5dc28
Aayush Bharda <aayushbharda999@gmail.com>
2025-12-13 16:14:00 +0530

    deploy changes

--- ai-habit-tracker/client/src/components/common/sidebar/SideBar.jsx
+++ ai-habit-tracker/client/src/components/common/sidebar/SideBar.jsx
@@ -11,7 +11,7 @@ import {
   FiInfo,
   FiLogOut,
 } from "react-icons/fi";
-import styles from "./Sidebar.module.css";
+import styles from "./sidebar.module.css";
 
 function Sidebar() {
   const logout = () => {
