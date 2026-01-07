95427e45dcc3ca364fa2e876c764dc4ffd072396
Aayush Bharda <aayushbharda999@gmail.com>
2026-01-07 16:11:04 +0530

    adding landing page

--- ai-habit-tracker/client/src/utils/protectedRoute.jsx
+++ ai-habit-tracker/client/src/utils/protectedRoute.jsx
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = () => {
+function ProtectedRoute() {
   const token = localStorage.getItem("token");
+
   return token ? <Outlet /> : <Navigate to="/login" />;
-};
+}
 
 export default ProtectedRoute;
